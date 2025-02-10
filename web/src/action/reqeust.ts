ad2d382dc53f219c56f16935f4004f6937e5ba0f
Teerapan Lerdtanaseangtham <garfild5512@gmail.com>
2025-02-10 10:43:22 +0700

    feat(web): box card input message

---
+++ web/src/action/reqeust.ts
@@ -0,0 +1,25 @@
+const fetchMsg = async () => {
+    const response = await fetch('/api/hello-world');
+    if(!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data;
+}
+
+const updateMsg = async (newMsg: string) => {
+    const response = await fetch('/api/hello-world', {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message: newMsg }),
+    });
+    if(!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data;
+}
+
+export { fetchMsg, updateMsg };
--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Prompt } from 'next/font/google'
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const fontPrompt = Prompt({
+  weight: ['400'],
+  subsets: ["latin"],
+  display: "swap",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,9 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${fontPrompt.className} antialiased`}
       >
-        {children}
+          {children}
       </body>
     </html>
   );
