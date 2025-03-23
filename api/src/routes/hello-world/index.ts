3f0218a0a5529d03a2baa4f4eb5f9e5cb986fbb3
tonkaew131 <nongtonkaew@gmail.com>
2025-03-23 17:27:17 +0700

    Removed unused route

--- api/src/routes/hello-world/index.ts
+++ api/src/routes/hello-world/index.ts
@@ -1,36 +1,36 @@
-import { helloWorldContract } from '@/contracts/HelloWorld';
+// import { helloWorldContract } from '@/contracts/HelloWorld';
 import Elysia, { t } from 'elysia';
 
 export const HelloWorldRoute = new Elysia({
     prefix: 'contracts/hello-world',
-})
-    .get(
-        '/',
-        async () => {
-            const message = await helloWorldContract.message();
+});
+// .get(
+//     '/',
+//     async () => {
+//         const message = await helloWorldContract.message();
 
-            return {
-                message: message,
-            };
-        },
-        { response: t.Object({ message: t.String() }) }
-    )
-    .post(
-        '/',
-        async (context) => {
-            const { message } = context.body;
+//         return {
+//             message: message,
+//         };
+//     },
+//     { response: t.Object({ message: t.String() }) }
+// )
+// .post(
+//     '/',
+//     async (context) => {
+//         const { message } = context.body;
 
-            const tx = await helloWorldContract.update(message);
-            await tx.wait();
+//         const tx = await helloWorldContract.update(message);
+//         await tx.wait();
 
-            return {
-                message: message,
-            };
-        },
-        {
-            body: t.Object({
-                message: t.String(),
-            }),
-            response: t.Object({ message: t.String() }),
-        }
-    );
+//         return {
+//             message: message,
+//         };
+//     },
+//     {
+//         body: t.Object({
+//             message: t.String(),
+//         }),
+//         response: t.Object({ message: t.String() }),
+//     }
+// );
