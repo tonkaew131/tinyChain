ad81d2c3230c9f42c8d38102318e12ccdf11dc77
tonkaew131 <nongtonkaew@gmail.com>
2025-03-23 17:55:17 +0700

    Add auto type casing

--- api/src/utils/auth.ts
+++ api/src/utils/auth.ts
@@ -1,10 +1,10 @@
 import { db } from '@/db';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: 'pg',
+        usePlural: true,
     }),
     emailAndPassword: {
         enabled: true,
