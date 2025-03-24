b420c52e37488bde7a1fc1c12c5a2cf8a2db5200
Athicha L. (mac) <nongtonkaew@gmail.com>
2025-03-24 16:37:27 +0700

    Merge openapi specs from better auth

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,10 +3,45 @@ import { Elysia } from 'elysia';
 import { AuthRoute } from './routes/auth';
 import { HelloWorldRoute } from './routes/hello-world';
 import { ProjectRoute } from './routes/project';
+import { auth } from './utils/auth';
 import swagger from '@elysiajs/swagger';
+import { merge } from 'openapi-merge';
 
 const app = new Elysia()
-    .use(swagger())
+    .use(
+        swagger({
+            scalarConfig: {
+                spec: {
+                    url: '/swagger/all-json',
+                },
+                servers: [
+                    {
+                        url: process.env.NEXT_PUBLIC_API_URL,
+                    },
+                ],
+            },
+        })
+    )
+    .get('/swagger/all-json', async () => {
+        const result = merge([
+            {
+                oas: await auth.api.generateOpenAPISchema(),
+                pathModification: {
+                    prepend: '/auth',
+                },
+            },
+            {
+                oas: await fetch(
+                    `${process.env.NEXT_PUBLIC_API_URL}/swagger/json`
+                ).then((res) => res.json()),
+                pathModification: {
+                    prepend: '',
+                },
+            },
+        ]);
+
+        return 'output' in result ? result.output : result;
+    })
     .use(AuthRoute)
     .use(ProjectRoute)
     .use(HelloWorldRoute)
